e64c7b3e28cc314d917ebdce8d1d7ee5c5ec6ad6
Gabriel Sarcevic <gabrielsarcevic1@gmail.com>
2025-04-28 18:47:37 +0200

    og to png

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,15 +18,15 @@
 	<meta name="og:description" content="Hi! I'm Gabriel." />
 	<meta name="og:type" content="website" />
 	<meta name="og:url" content="https://iamgabriel.dev" />
-	<meta name="og:image" content="https://iamgabriel.dev/og.webp" />
-	<meta name="og:image:type" content="image/webp" />
+	<meta name="og:image" content="https://iamgabriel.dev/og.png" />
+	<meta name="og:image:type" content="image/png" />
 	<meta name="og:image:width" content="1200" />
 	<meta name="og:image:height" content="630" />
 	<meta name="og:image:alt" content="IamGabriel_" />
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta name="twitter:width" content="1200" />
 	<meta name="twitter:height" content="630" />
-	<meta name="twitter:image" content="https://iamgabriel.dev/og.webp" />
+	<meta name="twitter:image" content="https://iamgabriel.dev/og.png" />
 	<meta name="twitter:image:alt" content="IamGabriel_" />
 	<meta name="twitter:title" content="IamGabriel" />
 	<meta name="twitter:url" content="https://iamgabriel.dev" />
