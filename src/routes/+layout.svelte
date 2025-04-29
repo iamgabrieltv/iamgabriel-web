465a71d25cacdac64b95f780493833f3d8a03904
Gabriel Sarcevic <gabrielsarcevic1@gmail.com>
2025-04-29 17:21:39 +0200

    responsive design for iPad

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -34,8 +34,8 @@
 	<meta name="theme-color" content="#05b805" />
 </svelte:head>
 
-<div class="flex h-screen w-screen flex-col px-2 py-5 md:items-center md:px-0 md:py-10">
-	<div class="h-full w-full md:w-1/3">
+<div class="flex h-screen w-screen flex-col px-2 py-5 md:items-center lg:px-0 lg:py-10">
+	<div class="h-full w-full lg:w-1/2 xl:w-1/3">
 		<nav class="mb-4 flex flex-row gap-2">
 			<a href="/" class="{path === '/' ? 'underlined' : ''} link">home</a>
 			<a href="/guestbook" class="{path === '/guestbook' ? 'underlined' : ''} link">guestbook</a>
