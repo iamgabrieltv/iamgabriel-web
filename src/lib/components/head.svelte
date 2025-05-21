2166f0ce04aab5d62cde4e9b723bf98abfdcdbd4
IamGabriel <gabrielsarcevic1@gmail.com>
2025-05-21 22:52:11 +0200

    move head to another file

---
+++ src/lib/components/head.svelte
@@ -0,0 +1,21 @@
+<meta name="description" content="Developer from Germany" />
+<meta name="author" content="IamGabriel" />
+<meta name="creator" content="IamGabriel" />
+<meta name="og:title" content="IamGabriel" />
+<meta name="og:description" content="Hi! I'm Gabriel." />
+<meta name="og:type" content="website" />
+<meta name="og:url" content="https://iamgabriel.dev" />
+<meta name="og:image" content="https://iamgabriel.dev/og.png" />
+<meta name="og:image:type" content="image/png" />
+<meta name="og:image:width" content="1200" />
+<meta name="og:image:height" content="630" />
+<meta name="og:image:alt" content="IamGabriel_" />
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:width" content="1200" />
+<meta name="twitter:height" content="630" />
+<meta name="twitter:image" content="https://iamgabriel.dev/og.png" />
+<meta name="twitter:image:alt" content="IamGabriel_" />
+<meta name="twitter:title" content="IamGabriel" />
+<meta name="twitter:url" content="https://iamgabriel.dev" />
+<meta name="twitter:description" content="Hi! I'm Gabriel." />
+<meta name="theme-color" content="#05b805" />
